4253b376ce285058af5dbeab1bc454aceeb569a2
tukY Chyvekshyno <onar2000@gmail.com>
2021-02-16 23:50:59 +0200

    describe cluster(of experts) class

--- cluster.py
+++ cluster.py
@@ -1,10 +1,27 @@
 import numpy as np
 
+
 class SimpleNPCluster:
     """A simple class describes cluster using numpy"""
-    def __init__(self):
-        pass
+
+    def __init__(self, o: np.ndarray):
+        self.values = []
+        self.values.append(o)
 
     def members(self):
-        pass
+        return self.values
 
+    def vshape(self):
+        return self.values[0].shape
+
+    def append(self, o: np.ndarray):
+        if self.vshape() != o.shape:
+            assert 0
+        self.values.append(o)
+
+    def vmean(self) -> np.ndarray:
+        res = np.zeros(self.values[0].shape)
+        for o in self.values:
+            res += o
+
+        return res / len(self.values)
--- clustering.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def find_cluster(matr):
-    pass
