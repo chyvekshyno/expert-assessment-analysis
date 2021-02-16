7cf59ee7a1af47c8c0a01c24ea440ede619ef34b
tukY Chyvekshyno <onar2000@gmail.com>
2021-02-16 23:51:45 +0200

    descibe fun distance

--- utils.py
+++ utils.py
@@ -1,11 +1,21 @@
-import numpy as np
+from cluster import SimpleNPCluster as npcluster
 
-def distance(v1, v2):
-    return 0
+
+def distance(cl1: npcluster, cl2: npcluster) -> float:
+    res = 0
+    v1 = cl1.vmean()
+    v2 = cl2.vmean()
+
+    for i in range(cl1.vshape()[0]):
+        for j in range(cl1.vshape()[1]):
+            res += abs(v1[i][j]-v2[i][j])
+
+    return res
+
 
 def mediana(matrix):
     return 0
 
-def vmean(varr):
-    return 0
 
+def find_cluster(matr):
+    pass
