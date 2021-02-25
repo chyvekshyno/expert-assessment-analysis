fix trust radius; add normalizing start data

main.py
@@ -2,6 +2,7 @@ import os
 
 from utils import *
 from cluster import SimpleNPCluster as npcluster, combine
+from data import expert_assessment
 
 file_name = "f.txt"
 dir_path = "out"
@@ -9,49 +10,11 @@ comp_path = os.path.join(dir_path, file_name)
 
 if __name__ == '__main__':
     # init parameters
-    expert_assessment = np.array([
-        [  # expert_1
-            [0.653, 0.223, 0.124],  # param_1
-            [0.321, 0.475, 0.204],  # param_2
-            [0.219, 0.188, 0.593]  # param_3
-        ], [  # expert_2
-            [0.299, 0.346, 0.355],  # param_1
-            [0.413, 0.248, 0.339],  # param_2
-            [0.136, 0.579, 0.285]  # param_3
-        ], [  # expert_3
-            [0.392, 0.208, 0.400],  # param_1
-            [0.717, 0.199, 0.084],  # param_2
-            [0.125, 0.594, 0.281]  # param_3
-        ], [  # expert_4
-            [0.382, 0.401, 0.217],  # param_1
-            [0.336, 0.462, 0.202],  # param_2
-            [0.094, 0.410, 0.496]  # param_3
-        ], [  # expert_5
-            [0.555, 0.169, 0.276],  # param_1
-            [0.292, 0.327, 0.381],  # param_2
-            [0.124, 0.435, 0.441]  # param_3
-        ], [  # expert_6
-            [0.310, 0.411, 0.279],  # param_1
-            [0.068, 0.316, 0.616],  # param_2
-            [0.317, 0.534, 0.149]  # param_3
-        ], [  # expert_7
-            [0.409, 0.443, 0.148],  # param_1
-            [0.402, 0.461, 0.137],  # param_2
-            [0.345, 0.201, 0.454]  # param_3
-        ], [  # expert_8
-            [0.729, 0.187, 0.084],  # param_1
-            [0.364, 0.500, 0.136],  # param_2
-            [0.327, 0.190, 0.483]  # param_3
-        ], [  # expert_9
-            [0.276, 0.322, 0.402],  # param_1
-            [0.530, 0.217, 0.253],  # param_2
-            [0.198, 0.389, 0.413]  # param_3
-        ]
-    ])
+
 
     # import into clusters
     expert_clusters = np.array([], dtype=npcluster)
-    for i in range(expert_assessment.shape[0]):
+    for i in range(norm(expert_assessment).shape[0]):
         expert_clusters = np.insert(expert_clusters,
                                     len(expert_clusters),
                                     [npcluster([str(i + 1)], [expert_assessment[i]])])

utils.py
@@ -16,10 +16,23 @@ def distance(cl1: npcluster, cl2: npcluster) -> float:
     return res
 
 
+def norm(table: np.ndarray) -> np.ndarray:
+    """
+    norm at axis=1
+    :param table: numpy array
+    :return: norm numpy array
+    """
+    res = table.copy()
+    for o in table:
+        array_sum = np.sum(o, axis=1)
+        o /= array_sum
+    return res
+
+
 def distance_matrix(morph_table: np.ndarray) -> np.ndarray:
     """
     calculate matrix if distances between clusters
-    :param morph_table: list of SimpleNPCluster s
+    :param morph_table: numpy array of SimpleNPCluster
     :return: np.array (dim=2)
     """
     mdim = len(morph_table)
@@ -50,7 +63,7 @@ def trust_radius(matrix: np.ndarray) -> int:
     mediana = matrix[argmediana(matrix)]
     trust_count = matrix.shape[0] // 2
     trust_ind = np.argsort(mediana)[:trust_count]
-    res = mediana[trust_ind[-1]] - mediana[trust_ind[0]]
+    res = mediana[trust_ind[-1]]
     return res
 
 
@@ -61,5 +74,5 @@ def find_cluster(matrix: np.ndarray) -> Tuple[int, int]:
     return coord
 
 
-def find_cluster_mediana(mediana: np.ndarray, ind : int) -> Tuple[int, int]:
+def find_cluster_mediana(mediana: np.ndarray, ind: int) -> Tuple[int, int]:
     return ind, np.nanargmin(mediana, axis=0).take(0)